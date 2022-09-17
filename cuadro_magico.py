74b2b587cd12b59ec30310275b1dfd761f721899
tatiana hoyos <tatiana98@gmail.com>
2022-09-17 18:35:47 -0500

    agrgando validacion correspondiente para realizar un cuadro magico

--- cuadro_magico.py
+++ cuadro_magico.py
@@ -1,4 +1,4 @@
-tamaño=int(input("ingrese tamaño de cuadro mágico "))
+tamaño=int(input("ingrese tamaño de cuadro mágico: "))
 
 #valido que le tamaño de la matriz sea valido
 while tamaño<3 or tamaño%2==0:
@@ -7,12 +7,46 @@ while tamaño<3 or tamaño%2==0:
 
 print("tamaño valido para cuadro mágico: ", tamaño)
 matriz=[]
-lista=[]
 
 #llenar con ceros la matriz
-for conador in range(0,tamaño):
-  lista.append(0)
-  matriz.append(lista)
-#se muestra la matrizz inicial
-for  dato in matriz:
+matriz=[[0 for i in range(tamaño)] for i in range(tamaño)]
+#mostrar matriz con ceros
+for dato in matriz:
+  print(dato)
+
+print("\nresultado:\n")
+#calculando punto de inicio
+fila=(int(tamaño/2-0.5))
+columna=0
+
+contador=1
+columna_anterior=0
+fila_anterior=0
+while (contador<=tamaño*tamaño):
+  matriz[fila][columna]=contador
+  contador=contador+1
+  #avanzar
+  columna_anterior=columna
+  fila_anterior=fila
+  fila=fila-1
+  columna=columna-1
+  
+  #validando si columna es negativa(por fuera de matriz)
+  if columna<0:
+    columna=tamaño-1
+  #validando si fila es negativa(fuera de la matriz)
+  if fila<0:
+    fila=tamaño-1
+  #se valida si la casilla esta ocupada
+  if matriz[fila][columna]!=0:
+    columna_anterior=columna_anterior+1
+    columna=columna_anterior
+    fila=fila_anterior
+  #valida la posicion en cero,cero va a la derecha
+  if columna==0 and fila==0 and matriz[0][0]!=0:
+    columna=columna+1
+
+
+#mostrar matriz modificada
+for dato in matriz:
   print(dato)
